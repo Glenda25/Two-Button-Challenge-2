43deb6f39b39a8746a0cbe501e2429a9c08cfa81
Glenda Mullan <glenda.mullan@gmail.com>
2023-01-20 12:44:05 +1100

    Initial Commit

---
+++ Two Button Challenge 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Button Challenge 2
+//
+//  Created by Glenda Mullan on 20/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
